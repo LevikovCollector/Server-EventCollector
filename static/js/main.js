50158953bc171c1837ac742ced73f0a802da2a4e
Vladimir Levikov <lemur1992@yandex.ru>
2017-11-04 16:35:21 +0300

    работа с id заменена на работу с классом
    работа с id заменена на работу с классом

--- static/js/main.js
+++ static/js/main.js
@@ -7,29 +7,29 @@ $(document).ready(function(){
         
         if(ch_chrome)
         {   
-            show_or_hide("th-google", "table.table.table-striped>tbody>tr.all-data>td#active_tab_url", 1)
+            show_or_hide("th-google", "table.table.table-striped>tbody>tr.all-data>td.js_active_tab_url", 1)
         }
         else
         {
-            show_or_hide("th-google", "table.table.table-striped>tbody>tr.all-data>td#active_tab_url", 0)
+            show_or_hide("th-google", "table.table.table-striped>tbody>tr.all-data>td.js_active_tab_url", 0)
         }
 
         if(ch_pycharm)
         {   
-            show_or_hide("th-pycharm", "table.table.table-striped>tbody>tr.all-data>td#pycharm_project", 1)
+            show_or_hide("th-pycharm", "table.table.table-striped>tbody>tr.all-data>td.js_pycharm_project", 1)
         }
         else
         {
-            show_or_hide("th-pycharm", "table.table.table-striped>tbody>tr.all-data>td#pycharm_project", 0)
+            show_or_hide("th-pycharm", "table.table.table-striped>tbody>tr.all-data>td.js_pycharm_project", 0)
         }
 
         if(ch_sublime)
         {   
-            show_or_hide("th-sublime", "table.table.table-striped>tbody>tr.all-data>td#sublime_path", 1)
+            show_or_hide("th-sublime", "table.table.table-striped>tbody>tr.all-data>td.js_sublime_path", 1)
         }
         else
         {
-            show_or_hide("th-sublime", "table.table.table-striped>tbody>tr.all-data>td#sublime_path", 0)
+            show_or_hide("th-sublime", "table.table.table-striped>tbody>tr.all-data>td.js_sublime_path", 0)
         }
 
     });
@@ -40,19 +40,19 @@ function show_or_hide(th_id, td_selector, show)
     if(show)
     {
         document.getElementById(th_id).className="table-all-data hidden-lg hidden-md hidden-sm"
-        var chrm_col = document.querySelectorAll(td_selector)
-        for(i=0;i<chrm_col.length;i++)
+        var col = document.querySelectorAll(td_selector)
+        for(i=0;i<col.length;i++)
         {
-            chrm_col[i].className="hidden-lg hidden-md hidden-sm"
+            col[i].className="hidden-lg hidden-md hidden-sm"
         }
     }
     else
     {
         document.getElementById(th_id).className="table-all-data"
-        var chrm_col = document.querySelectorAll(td_selector)
-        for(i=0;i<chrm_col.length;i++)
+        var col = document.querySelectorAll(td_selector)
+        for(i=0;i<col.length;i++)
         {
-            chrm_col[i].className="None"
+            col[i].className="None"
         }
     }
     
